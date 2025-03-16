3b8ae9f6207604c120456253d7f50fbb3afb29fe
Daniel <daniel.duczyminski@rossmann.com.pl>
2025-03-16 15:47:53 +0100

    first contract template

--- CompanyFormsManager/CompanyFormsManager/MainWindow.xaml.cs
+++ CompanyFormsManager/CompanyFormsManager/MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Markup;
+using System.Xml;
 
 namespace CompanyFormsManager
 {
@@ -19,6 +15,55 @@ namespace CompanyFormsManager
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnInputChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateTemplatePreview();
+        }
+
+        private void UpdateTemplatePreview()
+        {
+            /*
+            ContractNumberRun.Text = ContractNumberInput.Text;
+            DealDateRun.Text = DealDateInput.Text;
+            CustomerNameRun.Text = CustomerNameInput.Text;
+            CustomerAddressRun.Text = CustomerAddressInput.Text;
+            CustomerNIPRun.Text = CustomerNIPInput.Text;
+            CustomerPhoneNumberRun.Text = CustomerPhoneNumberInput.Text;
+            CustomerEmailRun.Text = CustomerEmailInput.Text;
+            */
+        }
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e)
+        {
+            PrintFlowDocumentDirectly();
+        }
+
+        private void PrintFlowDocumentDirectly()
+        {
+            FlowDocument originalDocument = MyDocument;
+
+            if (originalDocument == null)
+            {
+                MessageBox.Show("Błąd: Dokument nie istnieje!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string xaml = XamlWriter.Save(originalDocument);
+            StringReader stringReader = new StringReader(xaml);
+            XmlReader xmlReader = XmlReader.Create(stringReader);
+            FlowDocument clonedDocument = (FlowDocument)XamlReader.Load(xmlReader);
+
+            PrintDialog printDialog = new PrintDialog();
+
+            clonedDocument.PageHeight = printDialog.PrintableAreaHeight;
+            clonedDocument.PageWidth = printDialog.PrintableAreaWidth;
+            clonedDocument.PagePadding = new Thickness(50);
+            clonedDocument.ColumnGap = 0;
+            clonedDocument.ColumnWidth = printDialog.PrintableAreaWidth;
+
+            printDialog.PrintDocument(((IDocumentPaginatorSource)clonedDocument).DocumentPaginator, "Drukowanie umowy");
         }
     }
 }
